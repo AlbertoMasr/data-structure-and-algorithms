130bff2fae3343ea459dae67d0c465a7bcb81992
Manuel Alberto Sotelo Rivas <manuel.sr@culiacan.tecnm.mx>
2025-01-20 13:07:49 -0700

    [ADD] Common algorithms for binary search

--- algortihms/searching/Searching.java
+++ algortihms/searching/Searching.java
@@ -69,4 +69,89 @@ public class Searching {
             return binaryRecursive(list, middle + 1, high, number);
         }
     }
+
+    public int findFirstLastOcurrence(int[] list, int number, boolean first) {
+        int low = 0;
+        int high = list.length - 1;
+        int result = -1;
+        
+        while(low <= high) {
+            int middle = (low + high) / 2;
+            if(number == list[middle]) {
+                result = middle;
+                if(first) {
+                    high = middle - 1;
+                } else {
+                    low = middle + 1;
+                }
+            } else if(number < list[middle]) {
+                high = middle - 1;
+            } else {
+                low = middle + 1;
+            }
+        }
+        return result;
+    }
+
+    public int rotateArray(int[] list) {
+        int low = 0;
+        int high = list.length - 1;
+        while(low <= high) {
+
+            // Case 1
+            if(list[low] <= list[high]) {
+                return low;
+            }
+
+            int middle = (low + high) / 2;
+            int next = (middle + 1) % list.length;
+            int previous = (middle + list.length - 1) % list.length;
+
+            // Case 2
+            if(list[middle] <= list[next] && list[middle] <= list[previous]) {
+                return middle;
+            }
+            // Case 3
+            else if(list[middle] <= list[high]) {
+                high = middle - 1;
+            }
+            // Case 4
+            else if(list[middle] >= list[low]) {
+                low = middle + 1;
+            }
+
+        }
+        return -1;
+    }
+
+    public int findCircleArray(int[] list, int number) {
+        int low = 0;
+        int high = list.length - 1;
+        while(low <= high) {
+            int middle = (low + high) / 2;
+
+            // Case 1
+            if(list[middle] == number) {
+                return middle;
+            }
+
+            // Case 2
+            if(list[middle] <= list[high]) {
+                if(number > list[middle] && number <= list[high]) {
+                    low = middle + 1;
+                } else {
+                    high = middle - 1;
+                }
+            }
+            // Case 3
+            else {
+                if(number >= list[low] && number < list[middle]) {
+                    high = middle - 1;
+                } else {
+                    low = middle + 1;
+                }
+            }
+        }
+        return -1;
+    }
 }
--- algortihms/searching/SearchingHandler.java
+++ algortihms/searching/SearchingHandler.java
@@ -81,11 +81,130 @@ public class SearchingHandler {
         }
     }
 
+    public static void findFirstOcurrence(Searching searching) {
+        int[] list = {10, 24, 40, 40, 40, 73, 95, 100, 120, 140, 140, 140, 150, 160};
+        System.out.println("List: ");
+        print(list);
+
+        while (true) {
+            try {
+                System.out.println("Enter the number to find first occurrence: ");
+                int number = Integer.parseInt(scanner.nextLine());
+                int indexNumber = searching.findFirstLastOcurrence(list, number, true);
+
+                if (indexNumber != -1) {
+                    System.out.println("The number is in the index: " + indexNumber);
+                } else {
+                    System.out.println("The number is not in the list.");
+                }
+                break;
+            } catch (NumberFormatException e) {
+                System.err.println("Please, enter a number.");
+            }
+        }
+    }
+
+    public static void findLastOcurrence(Searching searching) {
+        int[] list = {10, 24, 40, 40, 40, 73, 95, 100, 120, 140, 140, 140, 150, 160};
+        System.out.println("List: ");
+        print(list);
+
+        while (true) {
+            try {
+                System.out.println("Enter the number to find last occurrence: ");
+                int number = Integer.parseInt(scanner.nextLine());
+                int indexNumber = searching.findFirstLastOcurrence(list, number, false);
+
+                if (indexNumber != -1) {
+                    System.out.println("The number is in the index: " + indexNumber);
+                } else {
+                    System.out.println("The number is not in the list.");
+                }
+                break;
+            } catch (NumberFormatException e) {
+                System.err.println("Please, enter a number.");
+            }
+        }
+    }
+
+    public static void countOcurrence(Searching searching) {
+        int[] list = {10, 24, 40, 40, 40, 40, 40, 73, 95, 95, 100, 120, 140, 140, 140, 150, 160};
+        System.out.println("List: ");
+        print(list);
+
+        while (true) {
+            try {
+                System.out.println("Enter the number to find the number of occurrence: ");
+                int number = Integer.parseInt(scanner.nextLine());
+                int startIndex = searching.findFirstLastOcurrence(list, number, true);
+
+                if(startIndex == -1) {
+                    System.out.println("The number "+ number +" has 0 occurrences.");
+                } else {
+                    int endIndex = searching.findFirstLastOcurrence(list, number, false);
+                    int count = endIndex - startIndex + 1;
+                    System.out.println("The number "+ number +" has "+ count +" occurrences.");
+                }
+                break;
+            } catch (NumberFormatException e) {
+                System.err.println("Please, enter a number.");
+            }
+        }
+    }
+
+    public static void rotateArray(Searching searching) {
+        // int[] list = {5, 6, 7, 8, 9, 7, 1, 2, 3, 4};
+        // int[] list = {7, 8, 9, 1, 2, 3, 4, 5, 6};
+        // int[] list = {1, 2, 3, 4};
+        // int[] list = {2, 3, 4, 1};
+        int[] list = {2, 1};
+        System.out.println("List: ");
+        print(list);
+
+        while (true) {
+            try {
+                int rotations = searching.rotateArray(list);
+                System.out.println("The array is rotated "+ rotations +" times.");
+                break;
+            } catch (NumberFormatException e) {
+                System.err.println("Please, enter a number.");
+            }
+        }
+    }
+
+    public static void finElementCircleArray(Searching searching) {
+        int[] list = {5, 6, 7, 8, 9, 1, 2, 3, 4};
+        System.out.println("List: ");
+        print(list);
+
+        while (true) {
+            try {
+                System.out.println("Enter the number to find: ");
+                int number = Integer.parseInt(scanner.nextLine());
+                int indexNumber = searching.findCircleArray(list, number);
+
+                if (indexNumber != -1) {
+                    System.out.println("The number is in the index: " + indexNumber);
+                } else {
+                    System.out.println("The number is not in the list.");
+                }
+                break;
+            } catch (NumberFormatException e) {
+                System.err.println("Please, enter a number.");
+            }
+        }
+    }
+
     public static void showMenu() {
         System.out.println("-----------------------------------");
         System.out.println("1. Linear search");
         System.out.println("2. Binary iterative search");
         System.out.println("3. Binary recursive search");
+        System.out.println("4. Find first ocurrence");
+        System.out.println("5. Find last ocurrence");
+        System.out.println("6. Count number of ocurrence");
+        System.out.println("7. How many times an array is rotated");
+        System.out.println("8. Find element in a circle array");
         System.out.println("-----------------------------------");
     }
 
@@ -106,6 +225,11 @@ public class SearchingHandler {
                     case 1 -> SearchingHandler.linear(searching);
                     case 2 -> SearchingHandler.binaryIterative(searching);
                     case 3 -> SearchingHandler.binaryRecursive(searching);
+                    case 4 -> SearchingHandler.findFirstOcurrence(searching);
+                    case 5 -> SearchingHandler.findLastOcurrence(searching);
+                    case 6 -> SearchingHandler.countOcurrence(searching);
+                    case 7 -> SearchingHandler.rotateArray(searching);
+                    case 8 -> SearchingHandler.finElementCircleArray(searching);
                     default -> System.out.println("Option doesn't support");
                 }
             } catch (NumberFormatException e) {
